5f30e9564f422df8d1f9ce0db74f6176886a72b4
nick-bennett <nbennett10@cnm.edu>
2024-09-23 13:51:17 -0600

    Change to enhanced for.

--- src/main/java/edu/cnm/deepdive/Main.java
+++ src/main/java/edu/cnm/deepdive/Main.java
@@ -3,8 +3,7 @@ package edu.cnm.deepdive;
 public class Main {
 
   public static void main(String[] args) {
-    for (int i = 0; i < args.length; i++) {
-      String arg = args[i];
+    for (String arg : args) {
       double c = Double.parseDouble(arg);
       double f = convertC2F(c);
       System.out.println(f);
