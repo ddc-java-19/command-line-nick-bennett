18e601b1c48c4eba196954c2c4b2c1821b9ed3b3
nick-bennett <nbennett10@cnm.edu>
2024-09-23 12:03:46 -0600

    Read celsius values from command line (in progress).

--- src/main/java/edu/cnm/deepdive/Main.java
+++ src/main/java/edu/cnm/deepdive/Main.java
@@ -3,7 +3,16 @@ package edu.cnm.deepdive;
 public class Main {
 
   public static void main(String[] args) {
-    System.out.println("Hello World!");
+    for (int i = 0; i < args.length; i++) {
+      String arg = args[i];
+      double c = Double.parseDouble(arg);
+      double f = convertC2F(c);
+    }
+    System.out.println(args.length);
+  }
+
+  public static double convertC2F(double celsius) {
+    return celsius * 9 / 5 + 32;
   }
 
 }
