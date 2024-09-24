2af58bea87ecc5a7bd1756c439d465137e23d5dc
nick-bennett <nbennett10@cnm.edu>
2024-09-24 11:03:49 -0600

    Using JUnit test methods and assertions.

--- src/test/java/edu/cnm/deepdive/MainTest.java
+++ src/test/java/edu/cnm/deepdive/MainTest.java
@@ -1,19 +1,30 @@
 package edu.cnm.deepdive;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
 public class MainTest {
 
   private static final double[] celsiusTemperatures = {-40, 0, 10, 20, 30, 50, 100};
   private static final double[] fahrenheitTemperatures = {-40, 32, 50, 68, 86, 122, 212};
   private static final double TOLERANCE = 0.000_000_000_1;
 
-  public static void main(String[] args) {
+  @Test
+  void convertC2F() {
     for (int i = 0; i < celsiusTemperatures.length; i++) {
       double celsius = celsiusTemperatures[i];
       double fahrenheit = fahrenheitTemperatures[i];
-      double diff = Math.abs(fahrenheit - Main.convertC2F(celsius));
-      assert diff <= TOLERANCE;
-      diff = Math.abs(celsius - Main.convertF2C(fahrenheit));
-      assert diff <= TOLERANCE;
+      assertEquals(fahrenheit, Main.convertC2F(celsius), TOLERANCE);
+    }
+  }
+
+  @Test
+  void convertF2C() {
+    for (int i = 0; i < celsiusTemperatures.length; i++) {
+      double celsius = celsiusTemperatures[i];
+      double fahrenheit = fahrenheitTemperatures[i];
+      assertEquals(celsius, Main.convertF2C(fahrenheit), TOLERANCE);
     }
   }
 
