9613a189130629cdd905515a616e03445cd1ac1b
nick-bennett <nbennett10@cnm.edu>
2024-09-24 16:10:18 -0600

    Revert "Replaced while with do-while loop."
    This reverts commit be7045fdbedb935b1313ebe0650960f2f1f077ce.

--- src/main/java/edu/cnm/deepdive/Main.java
+++ src/main/java/edu/cnm/deepdive/Main.java
@@ -7,7 +7,7 @@ public class Main {
     System.out.println("Converting from Celsius");
     TemperatureConverter converter = new TemperatureConverter();
     int i = 0;
-    do {
+    while (i < args.length) {
       String arg = args[i];
       switch (arg.toLowerCase().charAt(0)) {
         case 'c' -> {
@@ -26,7 +26,7 @@ public class Main {
         }
       }
       i++;
-    } while (i < args.length);
+    }
   }
 
 }
